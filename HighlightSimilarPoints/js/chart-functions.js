1fa5b20efe03bd6c9dc449f42815e3d6da63865b
Martin Kosterman <martin.kosterman@rivm.nl>
2019-05-15 16:50:55 +0200

    initial version Drupal: - sc_HTML-code.html contains htnl code for superscharts - sc_javascript.js contains all script to render 2 charts with synchronized highlighting - index modified to adapt to Drupal elements: new id's, added class ranglijst - chart-functions refactored so it's resembes sc code better; e.g. chart config in js file - Added Highcharts 4.04, which is used in Drupal

--- HighlightSimilarPoints/js/chart-functions.js
+++ HighlightSimilarPoints/js/chart-functions.js
@@ -1,71 +1,312 @@
 var aandoeningRanglijsten = [], aandoeningFilter = {};
 
+var chartConfig = {
+  "ranglijst_male": {
+    "chart": {
+      "type": "bar",
+      "renderTo": "ranglijst_male"
+    },
+    "colors": [
+      "rgba(0,80,149,0.4)"
+    ],
+    "title": {
+      "text": "mannen"
+    },
+    "xAxis": {
+      "visible": true,
+      "categories": [],
+      "lineWidth": 0,
+      "labels": {
+        "x": -5,
+        "align": "center",
+        "step": 1
+      },
+      "reversed": true
+    },
+    "yAxis": {
+      "opposite": true,
+      "title": {
+        "text": "Aantal sterfgevallen"
+      },
+      "labels": {},
+      "allowDecimals": false,
+      "min": 0,
+      "max": 15000,
+      "tickInterval": 2000
+    },
+    "tooltip": {},
+    "legend": {
+      "enabled": false
+    },
+    "plotOptions": {
+      "bar": {
+        "groupPadding": 0,
+        "borderWidth": 0.5,
+        "grouping": false
+      },
+      "series": {
+        "dataLabels": {
+          "enabled": true,
+          "align": "left"
+        }
+      }
+    },
+    "series": [
+      {
+        "id": "Mannen",
+        "name": "mannen",
+        "data": [
+          {
+            "y": 6134,
+            "name": "Longkanker"
+          },
+          {
+            "y": 5259,
+            "name": "Dementie"
+          },
+          {
+            "y": 4983,
+            "name": "Coronaire hartziekten"
+          },
+          {
+            "y": 3935,
+            "name": "Beroerte"
+          },
+          {
+            "y": 3759,
+            "name": "COPD"
+          },
+          {
+            "y": 3180,
+            "name": "Hartfalen"
+          },
+          {
+            "y": 2862,
+            "name": "Prostaatkanker"
+          },
+          {
+            "y": 2771,
+            "name": "Dikkedarmkanker"
+          },
+          {
+            "y": 1684,
+            "name": "Infecties van de onderste luchtwegen"
+          },
+          {
+            "y": 1623,
+            "name": "Accidentele val"
+          }
+        ],
+        "pointPadding": 0.05,
+        "showInLegend": true,
+        point: {
+          events: {
+            mouseOver: syncHighlight,
+            mouseOut: syncHighlight,
+            click: function (event) { 
+              showInfoTable(this.name); 
+            }
+          }
+        },
+      }
+    ]
+  },
+  "ranglijst_female": {
+    "chart": {
+      "type": "bar",
+      "renderTo": "ranglijst_female"
+    },
+    "colors": [
+      "rgba(0,80,149,0.4)"
+    ],
+    "title": {
+      "text": "vrouwen"
+    },
+    "xAxis": {
+      "visible": true,
+      "opposite": true,
+      "categories": [],
+      "labels": {
+        "enabled": false,
+        "step": 1
+      },
+      "reversed": true
+    },
+    "yAxis": {
+      "opposite": true,
+      "title": {
+        "text": "Aantal sterfgevallen"
+      },
+      "labels": {},
+      "allowDecimals": false,
+      "min": -15000,
+      "max": 0,
+      "tickInterval": 2000
+    },
+    "tooltip": {},
+    "legend": {
+      "enabled": false
+    },
+    "plotOptions": {
+      "bar": {
+        "groupPadding": 0,
+        "borderWidth": 0.5,
+        "grouping": false
+      },
+      "series": {
+        "dataLabels": {
+          "enabled": true,
+          "align": "right"
+        }
+      }
+    },
+    "series": [
+      {
+        "id": "Vrouwen",
+        "name": "vrouwen",
+        "data": [
+          {
+            "y": -10719,
+            "name": "Dementie"
+          },
+          {
+            "y": -5421,
+            "name": "Beroerte"
+          },
+          {
+            "y": -4509,
+            "name": "Hartfalen"
+          },
+          {
+            "y": -4257,
+            "name": "Longkanker"
+          },
+          {
+            "y": -3363,
+            "name": "COPD"
+          },
+          {
+            "y": -3350,
+            "name": "Coronaire hartziekten"
+          },
+          {
+            "y": -3106,
+            "name": "Borstkanker"
+          },
+          {
+            "y": -2408,
+            "name": "Accidentele val"
+          },
+          {
+            "y": -2326,
+            "name": "Dikkedarmkanker"
+          },
+          {
+            "y": -2085,
+            "name": "Infecties van de onderste luchtwegen"
+          }
+        ],
+        "pointPadding": 0.05,
+        "showInLegend": true,
+        point: {
+          events: {
+            mouseOver: syncHighlight,
+            mouseOut: syncHighlight,
+            click: function (event) { 
+              showInfoTable(this.name); 
+            }
+          }
+        },
+      }
+    ],
+    "exporting": {
+      "enabled": false
+    }
+  }
+};
+
+// Function for synchronized highlighting of data points with equal property
+function syncHighlight(event) {
+  eventType = event.type;
+
+  // event objects
+  point = event.target;
+  series = point.series;
+  chart = series.chart;
+
+  highlightColor = 'rgba(255,165,0,0.6)';//'orange';
+  if (eventType == 'mouseOut') highlightColor = series.options.color;
+  // if(point.color != series.color) 
+  //   highlightColor = series.options.color;
+  // }
+  highlightStyle = { color: highlightColor };
+  dimmStyle = { brightness: 0.5 };
+  // event indices
+  pointIndex = point.index;
+  seriesIndex = series.index;
+  chartIndex = chart.index;
+
+  seriesCount = event.target.series.chart.series.length;
+  // console.log(eventType + ' chart: ' + chartIndex + ' series: ' + seriesIndex + ' point: ' + pointIndex);
+
+  // Highlight points in all series with same 'name'
+  $.each(Highcharts.charts, function (index,  chart) {
+    $.each(chart.series, function () {
+      $.each(this.data, function () {
+        if (this.name == point.name || eventType == 'mouseOut') {
+          if (eventType == 'mouseOut') highlightColor = undefined;   //this.series.color;
+          highlightStyle = { color: highlightColor };
+          this.update(highlightStyle, true, false);
+        } else {
+          // this.update({color: 'lightgray'}, true, false);
+        }
+      })
+    })
+  })
+
+}
+
 function renderCharts() {
   /* Rendering all R Charts by looping all htmlwidget containers, 
   |  find correspondig R-config object (script.json) and render chart 
   
   */
-  $('div[id|="htmlwidget"').each(function (index, value) {
-    var container = this;
+  $('div.chart-container.ranglijst').each(function (index, value) {
     var id = $(this).attr('id');
-    var scriptTag = $('script[data-for="' + id + '"');
-    var strConfig = '', objConfig = {}, hcConfig = {};
 
     console.log('div' + index + ':' + id);
-
-    // download json if src attribute is present, otherwise use text content of script tag
-    if (scriptTag.attr('src') != undefined) {
-      $.getJSON(scriptTag.attr('src'), function (objConfig) {
-        if (objConfig.x != undefined) {
-          hcConfig = objConfig.x.hc_opts;
-          hcConfig = objRConfig.x.hc_opts;
-        } else {
-          hcConfig = objConfig;
-        }
-
-        // Add categroy numbers if no categories exist
-        if (hcConfig.xAxis.categories != undefined && hcConfig.xAxis.categories.length == 0) {
-          hcConfig.xAxis.categories = [];
-          $.each(hcConfig.series[0].data, function (item) {
-            hcConfig.xAxis.categories.push(item + 1);
-          });
-        }
-        Highcharts.chart(id, hcConfig);
-      });
-    } else {
-      strConfig = scriptTag.text();
-      objConfig = JSON.parse(strConfig);
-
-      console.log(objConfig);
-      if (objConfig.x != undefined) {
-        hcConfig = objConfig.x.hc_opts;
-        hcConfig = objRConfig.x.hc_opts;
-      } else {
-        hcConfig = objConfig;
-      }
-
-      Highcharts.chart(id, hcConfig);
-    }
-
+    
+    var chart = new Highcharts.Chart(chartConfig[id]);
 
   });
 }
 
-// Papaparse Test
-Papa.parse("data/AandoeningenEnRanglijsten.csv", {
-  download: true,
-  header: true,
-  complete: function (results) {
-    aandoeningRanglijsten = results.data;
-    
-  }
-});
-
 function showInfoTable(aandoening){
+  if(aandoeningRanglijsten.length == 0) {
+
+    // jQuery.ajaxSetup({async:true});
+
+    Papa.parse("data/AandoeningenEnRanglijsten.csv", {
+        download: true,
+        header: true,
+        complete: function (results) {
+          aandoeningRanglijsten = results.data;
+
+          renderTable(aandoening);
+          // jQuery.ajaxSetup({async:false});
+        }
+      });
+  } else {
+    renderTable(aandoening);
+  }
+}
+
+function renderTable(aandoening) {
   var aandoeningFilter = {
     prop: 'Aandoening',
     val: aandoening || 'Dementie'
   }
+
   var rankInLists = aandoeningRanglijsten.filter(function(item, index, filter){
     if (item[aandoeningFilter.prop] == aandoeningFilter.val) {
       return true;
